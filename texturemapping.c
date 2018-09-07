623d80f743cb98b0415a92f9d61da9e07ec2d429
roughtomato <baklazanzaglady@gmail.com>
2018-09-07 07:14:48 +0200

    Clean up

--- texturemapping.c
+++ texturemapping.c
@@ -1,19 +1,15 @@
-#include <stdlib.h>
-#include <string.h>
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glu.h>
-#include <stdio.h>
-#include <math.h>
-#include <unistd.h>
 
 #include "SDL.h"
 #include "SDL_image.h"
 
 #define RECTS 2
 #define COORDSETS 4
+#define TEXTURES 2
 
-GLuint texture[2];
+GLuint texture[TEXTURES];
 
 typedef struct Rect {
     GLfloat x[COORDSETS];
@@ -25,75 +21,61 @@ Rect rects[RECTS] = {
     {{-3.5, -3.5, -2.5, -2.5}, {1.0, -1.0, -1.0, 1.0}}
 };
 
+SDL_Surface *images[TEXTURES];
+const char *imageList[TEXTURES] = { "images/wick.jpg", "images/bladerunner.jpg" };
+
+void loadImages(void) {
+    for(int i = 0; i < TEXTURES; i++){
+        images[i] = IMG_Load(imageList[i]);
+        if(!images) {
+          printf("ERROR: couldn't load image %d\n", i);
+          exit(-1);
+        }
+    }
+}
+
 void init(void) {
     glClearColor (0.5, 0.5, 0.5, 0.0);
     glEnable(GL_DEPTH_TEST);
     glDepthFunc(GL_LESS);
-
-    SDL_Surface *image1 = IMG_Load("images/wick.jpg");
-    if (!image1) {
-        printf("ERROR: couldn't load image\n");
-        exit(-1);
-    }
-
-    SDL_Surface *image2 = IMG_Load("images/bladerunner.jpg");
-    if (!image1) {
-       printf("ERROR: couldn't load image\n");
-       exit(-1);
-    }
     glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-    // Create Texture
+    loadImages();
     glGenTextures(2, texture);
-    glBindTexture(GL_TEXTURE_2D, texture[0]);
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR); //scale linearly when image bigger than texture
-    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR); //scale linearly when image smaller than texture
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, image1->w, image1->h, 0,
-    GL_RGB, GL_UNSIGNED_BYTE, image1->pixels);
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
 
-    glBindTexture(GL_TEXTURE_2D, texture[1]);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-    glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
-    glTexImage2D(GL_TEXTURE_2D, 0, 3, image2->w, image2->h, 0,
-    GL_RGB, GL_UNSIGNED_BYTE, image2->pixels);
+    for(int i = 0; i < TEXTURES; i++){
+        glBindTexture(GL_TEXTURE_2D, texture[i]);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, images[i]->w, images[i]->h, 0,
+        GL_RGB, GL_UNSIGNED_BYTE, images[i]->pixels);
+        glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_DECAL);
+    }
+
     glEnable(GL_TEXTURE_2D);
     glShadeModel(GL_FLAT);
 }
 
 void display(void){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-    glBindTexture(GL_TEXTURE_2D, texture[0]);
-    glBegin(GL_QUADS);
-    glTexCoord2f(0.0, 0.0);
-    glVertex3f(rects[0].x[0],
-        rects[0].y[0], 0.0f);
-    glTexCoord2f(0.0, 1.0);
-    glVertex3f(rects[0].x[1],
-        rects[0].y[1], 0.0f);
-    glTexCoord2f(1.0, 1.0);
-    glVertex3f(rects[0].x[2],
-        rects[0].y[2], 0.0f);
-    glTexCoord2f(1.0, 0.0);
-    glVertex3f(rects[0].x[3],
-        rects[0].y[3], 0.0f);
-    glEnd();
-    glBindTexture(GL_TEXTURE_2D, texture[1]);
-    glBegin(GL_QUADS);
-    glVertex3f(rects[1].x[0],
-        rects[1].y[0], 0.0f);
-    glTexCoord2f(0.0, 1.0);
-    glVertex3f(rects[1].x[1],
-        rects[1].y[1], 0.0f);
-    glTexCoord2f(1.0, 1.0);
-    glVertex3f(rects[1].x[2],
-        rects[1].y[2], 0.0f);
-    glTexCoord2f(1.0, 0.0);
-    glVertex3f(rects[1].x[3],
-        rects[1].y[3], 0.0f);
-    glEnd();
+    for(int i = 0; i < TEXTURES; i++) {
+        if(i < RECTS) {
+            glBindTexture(GL_TEXTURE_2D, texture[i]);
+            glBegin(GL_QUADS);
+            glTexCoord2f(0.0, 0.0);
+            glVertex3f(rects[i].x[0],
+                rects[i].y[0], 0.0f);
+            glTexCoord2f(0.0, 1.0);
+            glVertex3f(rects[i].x[1],
+                rects[i].y[1], 0.0f);
+            glTexCoord2f(1.0, 1.0);
+            glVertex3f(rects[i].x[2],
+                rects[i].y[2], 0.0f);
+            glTexCoord2f(1.0, 0.0);
+            glVertex3f(rects[i].x[3],
+                rects[i].y[3], 0.0f);
+            glEnd();
+        }
+    }
     glutSwapBuffers();
 }
 
@@ -107,10 +89,17 @@ void reshape(int w, int h) {
     glTranslatef(0.0, 0.0, -3.6);
 }
 
+void freeSurfaces(void) {
+        for(int i = 0; i < TEXTURES; i++) {
+            SDL_FreeSurface(images[i]);
+        }
+}
+
 void keyboard (unsigned char key, int x, int y){
 
     switch (key) {
-       case 27: //“ESC” to exit the program TODO: free surfaces
+       case 27: //“ESC” to exit the program.
+       freeSurfaces();
        exit(0);
        break;
         default:
